322fe3fceb8cd4115506c2023324370c2df59374
Ivan Fang <40261483+FangBoHan@users.noreply.github.com>
2020-04-23 21:38:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from model import get_yolov2
-from dataprocessing import display_yolo
+from evaluate import display_yolo, get_yolo_result, get_COCO_mAP
+from dataprocessing import parse_annotation, parse_function, x1y1x2y2_xywh
 import glob
 
 
@@ -20,9 +21,10 @@ IMAGE_H, IMAGE_W = 416, 416
 GRID_H,  GRID_W  = 416 // 32, 416 // 32 # GRID size = IMAGE size / 32
 BOX              = 5
 CLASS            = len(LABELS)
-SCORE_THRESHOLD  = 0.5
+SCORE_THRESHOLD  = 0.6
 IOU_THRESHOLD    = 0.45
 ANCHORS          = [0.57273 ,  0.677385, 1.87446 ,  2.06253, 3.33843 ,  5.47434, 7.88282 ,  3.52778, 9.77052 ,  9.16828]
+IMAGE_COUNT      = 20000
 
 
 # 載入模型
@@ -34,3 +36,24 @@ x_files = glob.glob('data/image_416/*.jpg')
 
 for img_path in x_files:
     display_yolo(img_path, model, SCORE_THRESHOLD, IOU_THRESHOLD, ANCHORS)
+    
+# 計算模型在 COCO 2014 的表現
+ann_dir = "data/COCO_2014/annotations"
+img_dir = "data/COCO_2014/images"
+img_names, ture_boxes = parse_annotation(ann_dir, img_dir, LABELS)  # 這裡的 true_box 是 [x1, y1, x2, y2] 的格式
+imgs, ture_boxes = parse_function(img_names, ture_boxes)
+for i in range(len(ture_boxes)):    # 將 true_box 轉成 [x, y, w, h]
+    ture_boxes[i] = x1y1x2y2_xywh(ture_boxes[i])
+
+boxes = []
+scores = []
+classes = []
+for i in range(IMAGE_COUNT):
+    image_path = ""
+    b, s, c = get_yolo_result(image_path, model, SCORE_THRESHOLD, IOU_THRESHOLD, ANCHORS)
+    boxes.extend(b)
+    scores.extend(s)
+    classes.extend(c)
+    
+mAP = get_COCO_mAP(boxes, scores, classes, ture_boxes)
+print("mAP =", mAP)
